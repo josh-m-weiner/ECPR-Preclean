b968f4f7cd45ac64799a6dcfc31abb0834365454
josh-m-weiner <weinerjoshm@gmail.com>
2026-02-09 09:08:13 -0600

    Route Rmd HTML output to /output directory
    Add output_dir parameter to all render() calls in run_all.R and add
    /output to .gitignore.

---
+++ run_all.R
@@ -0,0 +1,20 @@
+# ==============================================================================
+# Run All: Full cleaning pipeline + table generation
+#
+# Renders all Rmd files in order:
+#   02_data_cleaning  ->  clean_data/data.RData
+#   03_impute         ->  clean_data/data_imputed.RData
+#   04_table1         ->  output/04_table1.html
+#   05_table2         ->  output/05_table2.html
+# ==============================================================================
+
+library(rmarkdown)
+
+render("02_data_cleaning.Rmd", output_dir = "output", quiet = TRUE)
+
+render("03_impute.Rmd", output_dir = "output", quiet = TRUE)
+
+render("04_table1.Rmd", output_dir = "output", quiet = TRUE)
+
+render("05_table2.Rmd", output_dir = "output", quiet = TRUE)
+
